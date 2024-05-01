ad58825113ad58da11632bb6978741b1dbcd8f71
Vinicius Stentzler <vin.stentzler@gmail.com>
2024-05-01 14:29:35 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
+
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [UsersModule, 
+    ReportsModule, 
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      synchronize: true,
+      entities: [User, Report],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
